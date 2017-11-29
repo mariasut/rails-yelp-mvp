96444c72376662306e061bfc9679391bfbcf939f
Maria Sut <mariasut17@gmail.com>
2017-11-29 12:50:29 +0100

    Adding seed to the models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+Review.destroy_all
+
+puts 'Creating restaurants...'
+10.times do
+  restaurant = Restaurant.create(name: Faker::Cat.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category:["chinese", "italian", "japanese", "french", "belgian"].sample)
+  puts "#{restaurant.name} saved!"
+end
+
+
+puts 'Creating reviews...'
+10.times do
+  review = Review.create(content: Faker::Lorem.paragraph, rating: [1, 2, 3, 4,5].sample,
+    restaurant_id: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].sample)
+  puts "#{review.content} saved!"
+end
+puts 'Finished!'
